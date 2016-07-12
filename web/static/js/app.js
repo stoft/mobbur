5c08f61a54d57534facee5187db6f4e9c2779163
Rasmus Larsson <rasmus.larsson@gmail.com>
2016-07-12 23:48:16 +0200

    Started playing with channels.

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -17,13 +17,14 @@ import "phoenix_html"
 //
 // Local files can be imported directly using relative
 // paths "./socket" or full ones "web/static/js/socket".
+import socket from "./socket"
 
-// import socket from "./socket"
 var elmDiv = document.getElementById('elm-app-lives-here');
 var app = Elm.Main.embed(elmDiv);
 
 // var audio = new Audio('/audio/start1.mp3')
 
 app.ports.alarm.subscribe(function() {
-  document.getElementById('alarm').play()
+  document.getElementById('alarm').play();
+  socket.sendStatus("foo");
 });
