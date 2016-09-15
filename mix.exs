7e03c0b7cf47ed5c39bc96844c693e5f165b89c6
Rasmus Larsson <rasmus.larsson@gmail.com>
2016-09-15 09:01:46 +0200

    Changed postgrex dips

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule Mobbur.Mixfile do
   defp deps do
     [{:phoenix, "~> 1.2.0"},
      {:phoenix_pubsub, "~> 1.0"},
-     {:postgrex, ">= 0.0.0"},
+     {:postgrex, "~> 0.12.0"},
      {:phoenix_ecto, "~> 3.0-rc"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
